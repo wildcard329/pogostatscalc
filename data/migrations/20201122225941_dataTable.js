9313bff17c00fa41668dc184bbd9182a1909f64b
wildcard329 <enkiduwildman@gmail.com>
2020-11-22 23:22:38 -0800

    initialized table

---
+++ data/migrations/20201122225941_dataTable.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('pokemon', pokemon => {
+        pokemon.integer('pokemon_id')
+                .unique()
+                .notNullable();
+        pokemon.text('pokemon_name')
+                .unique()
+                .notNullable();
+        pokemon.integer('pokemon_attack')
+                .notNullable();
+        pokemon.integer('pokemon_defense')
+                .notNullable();
+        pokemon.integer('pokemon_stamina')
+                .notNullable();
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('pokemon');
+};
